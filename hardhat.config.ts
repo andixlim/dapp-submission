9af073341c38e54c0eeb36a4c81031caf6c9f98d
andixlim <147823509+andixlim@users.noreply.github.com>
2024-03-26 15:45:21 +0800

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.VITE_JSON_RPC_URL,
+      accounts: [process.env.VITE_PRIVATE_KEY],
+    },
+  },
+
+  sourcify: {
+    enabled: true
+  }
+};
